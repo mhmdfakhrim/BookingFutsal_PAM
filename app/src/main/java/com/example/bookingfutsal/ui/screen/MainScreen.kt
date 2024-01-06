0da998155b8d5d0442a435744b9b79537a577cac
mhmdfakhrim <muhfakhri4@gmail.com>
2024-01-06 23:29:57 +0700

    initial commit

--- app/src/main/java/com/example/bookingfutsal/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/bookingfutsal/ui/screen/MainScreen.kt
@@ -9,31 +9,61 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.bookingfutsal.nav.Screens
 import com.example.bookingfutsal.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
     navController: NavController,
 ){
     Column(
         modifier = Modifier
-            .padding(start = 50.dp, end = 50.dp)
             .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Image(painter = painterResource(id = R.drawable.lpg_futsal), contentDescription = null)
+    ) {
+        TopAppBar(
+            modifier = Modifier.fillMaxWidth(),
+            title = {
+                Text(
+                    text = "Home",
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = FontStyle.Normal
+                    ),
+                    color = Color.White
+                )
+            },
+            colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.DarkGray)
+        )
+        Column(
+            modifier = Modifier
+                .padding(start = 60.dp, end = 60.dp, bottom = 50.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(painter = painterResource(id = R.drawable.lpg_futsal), contentDescription = null)
 
-        Spacer(modifier = Modifier.height(70.dp))
+            Spacer(modifier = Modifier.height(70.dp))
 
             // Tombol lihat pesanan
             Button(
@@ -56,6 +86,6 @@ fun MainScreen(
             ) {
                 Text(text = "Pesan Lapangan")
             }
+        }
     }
-
 }
