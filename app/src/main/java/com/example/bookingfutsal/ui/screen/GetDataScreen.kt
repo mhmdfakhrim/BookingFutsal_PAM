e4d5cf6a695a1c21e089d5b338b6bfb9996acd83
mhmdfakhrim <muhfakhri4@gmail.com>
2024-01-07 19:00:45 +0700

    initial commit

--- app/src/main/java/com/example/bookingfutsal/ui/screen/GetDataScreen.kt
+++ app/src/main/java/com/example/bookingfutsal/ui/screen/GetDataScreen.kt
@@ -81,7 +81,7 @@ fun GetDataScreen(
         // get data Layout
         Column(
             modifier = Modifier
-                .padding(start = 40.dp, end = 40.dp, bottom = 50.dp)
+                .padding(start = 40.dp, end = 40.dp, bottom = 30.dp)
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
